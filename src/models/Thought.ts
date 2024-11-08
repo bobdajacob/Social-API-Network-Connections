ab1d22238737cf1cab135fcb4e2b615b815599a0
Bob <bobjacob23@yahoo.com>
2024-11-07 17:05:40 -0800

    did thoughts model and reaction schema

---
+++ src/models/Thought.ts
@@ -0,0 +1,61 @@
+import { Schema, Document, model, ObjectId } from 'mongoose';
+
+interface IReaction extends Document {
+  reactionId: ObjectId[];
+  reactionBody: String;
+  username: String;
+  createdAt: Date;
+}
+
+interface IThought extends Document {
+  thoughtText: string,
+  createdAt: Date,
+  username: string,
+  reactions: IReaction[];
+}
+
+const reactionSchema = new Schema<IReaction>({
+  reactionId: [{
+    type: Schema.Types.ObjectId,
+  }],
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280, 
+  },
+  username: {
+    type: String,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAt: any) => createdAt.toLocaleString()
+  }
+});
+
+const thoughtSchema = new Schema<IThought>({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAt: any) => createdAt.toLocaleString()
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [reactionSchema]
+});
+
+thoughtSchema.virtual('reactionCount').get(function (this: IThought) {
+  return this.reactions.length;
+})
+
+const Thought = model<IThought>('Thought', thoughtSchema);
+export default Thought;
--- src/models/User.ts
+++ src/models/User.ts
@@ -9,8 +9,18 @@ interface IUser extends Document {
 
 const userSchema = new Schema<IUser>(
   {
-    username: String,
-    email: String,
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
     thoughts: [{
       type: Schema.Types.ObjectId,
       ref: 'Thought'
